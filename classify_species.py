ea03790401c8f26c2c05cde522b35ef1033b8532
Stephanie Blossom <sblossom@asu.edu>
2023-05-08 22:01:26 -0700

    Update classify_species.py

--- classify_species.py
+++ classify_species.py
@@ -37,9 +37,9 @@ test_loader = DataLoader(test_dataset, batch_size=16)
 class IrisClassifier(nn.Module):
     def __init__(self):
         super(IrisClassifier, self).__init__()
-        self.fc1 = nn.Linear(4, 64)
-        self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, 3)
+        self.fc1 = nn.Linear(4, 64) //The first linear layer self.fc1 takes an input of size 4 (representing the features of the iris) and produces an output of size 64
+        self.fc2 = nn.Linear(64, 64) //The second linear layer self.fc2 takes an input of size 64 and produces an output of size 64.
+        self.fc3 = nn.Linear(64, 3) //The third linear layer self.fc3 takes an input of size 64 and produces an output of size 3 (representing the three classes of iris flowers).
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
